8ff3bf5a400b835b3138d6f166cf1dff7f3f4749
LEGENDANISH <0504anish@gmail.com>
2026-06-20 13:18:54 +0530

    fixed the ui

--- frontend/src/components/chat/index.ts
+++ frontend/src/components/chat/index.ts
@@ -2,10 +2,11 @@
 export { ChatPage } from "./ChatPage";
 export { ChatHeader } from "./ChatHeader";
 export { MessageList } from "./MessageList";
-export { MessageBubble, SystemMessage, PlanCard } from "./MessageBubble";
+export { MessageBubble, SystemMessage } from "./MessageBubble"; // Removed PlanCard
 export { ChatInput } from "./ChatInput";
 export { AgentThinking } from "./AgentThinking";
 export { Avatar } from "./Avatar";
 export { MentionDropdown } from "./MentionDropdown";
 export { EmptyState } from "./EmptyState";
+export { PlanCard } from "./PlanCard";
 export { AGENT_CONFIG, ALL_AGENTS, getAgentConfig } from "./AgentConfig";
